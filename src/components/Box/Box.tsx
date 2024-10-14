dd85d3832c6cda213be24c48ba9a7576ce2d384b
David Rappa <davidrappa@outlook.com>
2024-10-14 16:25:05 -0300

    feat :sparkles: added box component with restyle

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,34 @@
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+
+import {Theme} from '../../theme/theme';
+
+export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  TouchableOpacityProps;
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, layout, border, spacingShorthand],
+  TouchableOpacity,
+);
