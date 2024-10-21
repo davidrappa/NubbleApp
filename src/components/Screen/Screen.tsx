ae041432bb5956b188a1b24f67bdf6d683082eb5
David Rappa <davidrappa@outlook.com>
2024-10-21 09:35:44 -0300

    feat :sparkles: added react navigation to login flow

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {KeyboardAvoidingView, Platform} from 'react-native';
-import {Box} from '../Box/Box';
+import {Box, TouchableOpacityBox} from '../Box/Box';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
 import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
 import {useAppTheme} from '../../hooks/useAppTheme';
+import {useNavigation} from '@react-navigation/native';
 
 interface ScreenProps {
   children: React.ReactNode;
@@ -21,6 +22,8 @@ export function Screen({
   const {top, bottom} = useAppSafeArea();
   const {colors} = useAppTheme();
 
+  const navigation = useNavigation();
+
   const Container = scrollable ? ScrollViewContainer : ViewContainer;
 
   return (
@@ -33,12 +36,16 @@ export function Screen({
           paddingHorizontal="s24"
           style={{paddingTop: top, paddingBottom: bottom}}>
           {canGoBack && (
-            <Box flexDirection="row" mb="s24" alignItems="center">
+            <TouchableOpacityBox
+              flexDirection="row"
+              mb="s24"
+              alignItems="center"
+              onPress={navigation.goBack}>
               <Icon name="arrowLeft" color="primary" />
               <Text preset="paragraphMedium" semiBold ml="s8">
                 Voltar
               </Text>
-            </Box>
+            </TouchableOpacityBox>
           )}
           {children}
         </Box>
