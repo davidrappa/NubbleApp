07a29bcedbea5a45379dbdc9e40ed92e652fd396
David Rappa <davidrappa@outlook.com>
2024-10-22 15:16:17 -0300

    refactor :hammer: refactored screen with react hook form and inputs

--- src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen.tsx
@@ -1,16 +1,39 @@
+import {zodResolver} from '@hookform/resolvers/zod';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React from 'react';
+import {useForm} from 'react-hook-form';
 import {Button} from '../../../components/Button/Button';
+import {FormTextInput} from '../../../components/Form/FormTextInput';
 import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
-import {TextInput} from '../../../components/TextInput/TextInput';
 import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess';
+import {RootStackParamList} from '../../../routes/Routes';
 
-export function ForgotPasswordScreen() {
+import {
+  ForgotPasswordSchema,
+  forgotPasswordSchema,
+} from './forgotPasswordSchema';
+
+type ScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'ForgotPasswordScreen'
+>;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function ForgotPasswordScreen({navigation}: ScreenProps) {
   const {reset} = useResetNavigationSuccess();
-  function submitForm() {
-    // TODO: submit form
+
+  const {control, formState, handleSubmit} = useForm<ForgotPasswordSchema>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: '',
+    },
+    mode: 'onChange',
+  });
+
+  function submitForm(values: ForgotPasswordSchema) {
+    console.log(values);
     reset({
-      title: `Enviamos as instruções para seu  ${'\n'}e-mail`,
+      title: `Enviamos as instruções ${'\n'}para seu e-mail`,
       description:
         'Clique no link enviado no seu e-mail para recuperar sua senha',
       icon: {
@@ -27,12 +50,20 @@ export function ForgotPasswordScreen() {
       <Text preset="paragraphLarge" mb="s32">
         Digite seu e-mail e enviaremos as instruções para redefinição de senha
       </Text>
-      <TextInput
+
+      <FormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{mb: 's40'}}
       />
-      <Button onPress={submitForm} title="Recuperar senha" />
+
+      <Button
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+        title="Recuperar senha"
+      />
     </Screen>
   );
 }
--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+
+import {Button} from '../../../components/Button/Button';
+import {FormPasswordInput} from '../../../components/Form/FormPasswordInput';
+import {FormTextInput} from '../../../components/Form/FormTextInput';
+
 import {Screen} from '../../../components/Screen/Screen';
 import {Text} from '../../../components/Text/Text';
-import {TextInput} from '../../../components/TextInput/TextInput';
-import {Button} from '../../../components/Button/Button';
-import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {useResetNavigationSuccess} from '../../../hooks/useResetNavigationSuccess';
+import {RootStackParamList} from '../../../routes/Routes';
+import {signUpSchema, SignUpSchema} from './signUpSchema';
 
-export function SignUpScreen() {
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function SignUpScreen({navigation}: ScreenProps) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const {reset} = useResetNavigationSuccess();
-
-  function submitForm() {
-    // TODO: implementar
+  const {control, formState, handleSubmit} = useForm<SignUpSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      fullName: '',
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+  function submitForm(formValues: SignUpSchema) {
+    console.log(formValues);
     reset({
       title: 'Sua conta foi criada com sucesso!',
-      description: 'Agora é so fazer login na nossa plataforma.',
+      description: 'Agora é só fazer login na nossa plataforma',
       icon: {
         name: 'checkRound',
         color: 'success',
@@ -26,25 +46,43 @@ export function SignUpScreen() {
         Criar uma conta
       </Text>
 
-      <TextInput label="Seu username" placeholder="@" boxProps={{mb: 's20'}} />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="username"
+        label="Seu username"
+        placeholder="@"
+        boxProps={{mb: 's20'}}
+      />
+
+      <FormTextInput
+        control={control}
+        name="fullName"
+        autoCapitalize="words"
         label="Nome Completo"
         placeholder="Digite seu nome completo"
         boxProps={{mb: 's20'}}
       />
-      <TextInput
+      <FormTextInput
+        control={control}
+        name="email"
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{mb: 's20'}}
       />
 
-      <PasswordInput
+      <FormPasswordInput
+        control={control}
+        name="password"
         label="Senha"
         placeholder="Digite sua senha"
         boxProps={{mb: 's48'}}
       />
 
-      <Button onPress={submitForm} title="Criar uma conta" />
+      <Button
+        disabled={!formState.isValid}
+        onPress={handleSubmit(submitForm)}
+        title="Criar uma conta"
+      />
     </Screen>
   );
 }
