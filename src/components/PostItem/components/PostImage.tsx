5efb9e979595f7e691b77969a34f10c18d8e2854
David Rappa <davidrappa@outlook.com>
2025-01-16 18:15:51 -0300

    feat: added post bottom

--- src/components/PostItem/components/PostImage.tsx
+++ src/components/PostItem/components/PostImage.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Dimensions, Image} from 'react-native';
+import {Dimensions, Image, ImageStyle} from 'react-native';
 
 import {Post} from '@domain';
 
 type Props = Pick<Post, 'imageURL'>;
 
 export function PostImage({imageURL}: Props) {
-  return (
-    <Image
-      resizeMode="cover"
-      source={{uri: imageURL}}
-      style={{width: Dimensions.get('screen').width, height: 300}}
-    />
-  );
+  return <Image resizeMode="cover" source={{uri: imageURL}} style={$image} />;
 }
+
+const $image: ImageStyle = {
+  width: Dimensions.get('screen').width,
+  height: 300,
+  marginHorizontal: -24,
+};
