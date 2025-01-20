03be1126bc63276f1642b8eb57bf94d01457be20
David Rappa <davidrappa@outlook.com>
2025-01-20 16:11:33 -0300

    feat: created hook to get paginated list

---
+++ src/domain/hooks/usePaginatedList.ts
@@ -0,0 +1,72 @@
+import {useEffect, useState} from 'react';
+
+import {Page} from '@types';
+
+export function usePaginatedList<Data>(
+  getList: (page: number) => Promise<Page<Data>>,
+) {
+  const [list, setList] = useState<Data[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<unknown>(null);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  async function fetchInitialData() {
+    try {
+      setError(null);
+      setLoading(true);
+
+      const {data, meta} = await getList(1);
+
+      setList(data);
+
+      if (meta.hasNextPage) {
+        setPage(2);
+        setHasNextPage(true);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function fetchNextPage() {
+    if (loading || !hasNextPage) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const {data, meta} = await getList(page);
+
+      setList(prev => [...prev, ...data]);
+
+      if (meta.hasNextPage) {
+        setPage(prev => prev + 1);
+      } else {
+        setHasNextPage(false);
+      }
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchInitialData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    loading,
+    error,
+    list,
+    refresh: fetchInitialData,
+    fetchNextPage,
+  };
+}
