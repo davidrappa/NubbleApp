5569ffa53ceff9aaef077f60e5113d44ddfa6bbc
David Rappa <davidrappa@outlook.com>
2025-01-20 16:11:55 -0300

    refactor: update use post list using paginated hook

--- src/domain/Post/useCases/usePostList.ts
+++ src/domain/Post/useCases/usePostList.ts
@@ -1,63 +1,8 @@
-import {useEffect, useState} from 'react';
+import {usePaginatedList} from '@domain';
 
-import {Post, postService} from '@domain';
+import {postService} from '../postService';
+import {Post} from '../postTypes';
 
 export function usePostList() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<unknown>(null);
-  const [postList, setPostList] = useState<Post[]>([]);
-  const [page, setPage] = useState(1);
-  const [hasNextPage, setHasNextPage] = useState(true);
-
-  async function fetchInitialData() {
-    try {
-      setError(null);
-      setLoading(true);
-
-      const {data, meta} = await postService.getList(1);
-
-      setPostList(data);
-
-      if (meta.hasNextPage) {
-        setPage(2);
-        setHasNextPage(true);
-      } else {
-        setHasNextPage(false);
-      }
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function fetchNextPage() {
-    if (loading || !hasNextPage) {
-      return;
-    }
-
-    try {
-      setLoading(true);
-
-      const {data, meta} = await postService.getList(page);
-
-      setPostList(prev => [...prev, ...data]);
-
-      if (meta.hasNextPage) {
-        setPage(prev => prev + 1);
-      } else {
-        setHasNextPage(false);
-      }
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchInitialData();
-  }, []);
-
-  return {loading, error, postList, refresh: fetchInitialData, fetchNextPage};
+  return usePaginatedList<Post>(postService.getList);
 }
