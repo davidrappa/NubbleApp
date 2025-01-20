77f93a9fc486ec2d78428dfc08752019b27b62b7
David Rappa <davidrappa@outlook.com>
2025-01-20 16:12:40 -0300

    feat: added empty component to home screen and added hook to get posts

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,38 +1,56 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, ListRenderItemInfo, StyleProp, ViewStyle} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  FlatList,
+  ListRenderItemInfo,
+  RefreshControl,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 
-import {Post, postService} from '@domain';
-import {AppTabScreenProps} from 'src/routes/navigationType';
+import {Post, usePostList} from '@domain';
+import {useScrollToTop} from '@react-navigation/native';
 
 import {PostItem, Screen} from '@components';
+import {AppTabScreenProps} from '@routes';
 
+import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
-  const [postList, setPostList] = useState<Post[]>([]);
+export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+  const {error, loading, list, refresh, fetchNextPage} = usePostList();
+
+  const flatListRef = useRef<FlatList<Post>>(null);
+  useScrollToTop(flatListRef);
 
   function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
   }
 
-  useEffect(() => {
-    postService.getList().then(list => setPostList(list));
-  }, []);
-
   return (
     <Screen style={$screen}>
       <FlatList
-        data={postList}
+        ref={flatListRef}
+        data={list}
         ListHeaderComponent={<HomeHeader />}
         keyExtractor={item => item.id}
         renderItem={renderItem}
+        onEndReached={fetchNextPage}
+        onEndReachedThreshold={0.1}
+        refreshing={loading}
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={refresh} />
+        }
+        contentContainerStyle={{flex: list.length === 0 ? 1 : undefined}}
+        ListEmptyComponent={
+          <HomeEmpty loading={loading} error={error} refetch={refresh} />
+        }
       />
     </Screen>
   );
 }
 
 const $screen: StyleProp<ViewStyle> = {
+  flex: 1,
   paddingTop: 0,
   paddingBottom: 0,
   paddingHorizontal: 0,
