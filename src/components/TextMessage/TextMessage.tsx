6561f03f9a44eef92afbac3bdc761bc6038c1430
David Rappa <davidrappa@outlook.com>
2025-01-21 11:43:23 -0300

    feat: added ProfileAvatar and TextMessage components

---
+++ src/components/TextMessage/TextMessage.tsx
@@ -0,0 +1,56 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+
+import {Box} from '../Box/Box';
+import {Text} from '../Text/Text';
+import {$textInputStyle} from '../TextInput/TextInput';
+
+interface TextMessageProps extends RNTextInputProps {
+  onPressSend: () => void;
+}
+export function TextMessage({
+  onPressSend,
+  value,
+  ...rnTextInputProps
+}: TextMessageProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  const sendIsDisabled = value?.trim().length === 0;
+
+  return (
+    <Pressable onPress={focusInput}>
+      <Box
+        flexDirection="row"
+        paddingHorizontal="s16"
+        paddingVertical="s14"
+        justifyContent="space-between"
+        alignItems="center"
+        backgroundColor="gray5"
+        borderRadius="s12">
+        <RNTextInput
+          ref={inputRef}
+          value={value}
+          placeholderTextColor={colors.gray2}
+          style={[$textInputStyle, {color: colors.gray1}]}
+          {...rnTextInputProps}
+        />
+        <Pressable onPress={onPressSend} disabled={sendIsDisabled}>
+          <Text bold color={sendIsDisabled ? 'gray2' : 'primary'}>
+            Enviar
+          </Text>
+        </Pressable>
+      </Box>
+    </Pressable>
+  );
+}
