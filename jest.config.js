bb336147268ed27fbb89505b0b09c7d00faa2958
David Rappa <davidrappa@outlook.com>
2025-01-28 15:43:00 -0300

    tests: added jest config, and customRender

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
+  modulePathIgnorePatterns: ['.*/mockedData/.*'],
 };
