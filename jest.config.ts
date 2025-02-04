752788f4f8b9b7bc657b1a412634885ab4bca2e4
David Rappa <davidrappa@outlook.com>
2025-02-04 17:48:11 -0300

    test: added search screen integration test

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,16 @@ import type {Config} from 'jest';
 const config: Config = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
-  collectCoverageFrom: [
-    'src/{components,utils,hooks,domain,screen}/**/*.{js,jsx,ts,tsx}',
-  ],
-  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
+  testTimeout: 15000,
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain,screens}/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context|@react-navigation)/)',
   ],
 };
 
