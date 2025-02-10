29a914fc92131e7e5f46dda0e415a639b4bf4f98
David Rappa <davidrappa@outlook.com>
2025-02-10 10:55:56 -0300

    feat: added new post screen

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
+import {useCameraRoll} from '@services';
 import {AppTabScreenProps} from 'src/routes/navigationType';
 
-import {Button, Screen, Text} from '@components';
+import {PressableBox, Screen} from '@components';
+
+import {Header} from './components/Header';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const ITEM_WIDTH = SCREEN_WIDTH / 4;
 
 export function NewPostScreen({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   navigation,
 }: AppTabScreenProps<'NewPostScreen'>) {
+  const [selectedImage, setSelectedImage] = useState<string>();
+  const {photoList, fetchNextPage} = useCameraRoll(true, setSelectedImage);
+
+  const flatListRef = useRef<FlatList>(null);
+
+  function onSelectImage(imageUri: string) {
+    setSelectedImage(imageUri);
+    flatListRef.current?.scrollToOffset({offset: 0, animated: true});
+  }
+
+  function renderItem({item}: ListRenderItemInfo<string>) {
+    return (
+      <PressableBox onPress={() => onSelectImage(item)}>
+        <Image
+          source={{uri: item}}
+          style={{width: ITEM_WIDTH, height: ITEM_WIDTH}}
+        />
+      </PressableBox>
+    );
+  }
+
   return (
-    <Screen>
-      <Text>NewPost Screen</Text>
-      <Button
-        title={'Ir para as configurações'}
-        onPress={() => navigation.navigate('SettingsScreen')}
+    <Screen canGoBack noPaddingHorizontal title="Novo post">
+      <FlatList
+        ref={flatListRef}
+        numColumns={4}
+        data={photoList}
+        onEndReached={fetchNextPage}
+        onEndReachedThreshold={0.1}
+        renderItem={renderItem}
+        ListHeaderComponent={
+          <Header imageWidth={SCREEN_WIDTH} imageUri={selectedImage} />
+        }
       />
     </Screen>
   );
---
+++ src/screens/app/NewPostScreen/components/Header.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {ImageBackground, StyleSheet} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+
+import {Box, BoxProps, Button, Icon, Text} from '@components';
+
+interface Props {
+  imageUri?: string;
+  imageWidth: number;
+}
+
+export function Header({imageUri, imageWidth}: Props) {
+  const navigation = useNavigation();
+
+  function navigateToPublishPost() {
+    if (imageUri) {
+      navigation.navigate('PublishPostScreen', {imageUri});
+    }
+  }
+
+  return (
+    <Box>
+      <ImageBackground
+        source={{uri: imageUri}}
+        style={[
+          {width: imageWidth, height: imageWidth},
+          styles.imageBackground,
+        ]}>
+        {Boolean(imageUri) && (
+          <Button
+            title="Escolher essa"
+            mb="s24"
+            onPress={navigateToPublishPost}
+            preset="ghost"
+          />
+        )}
+      </ImageBackground>
+      <Box {...$optionsStyle}>
+        <Text preset="headingSmall">Sua galeria</Text>
+        <Icon name="camera" />
+      </Box>
+    </Box>
+  );
+}
+
+const $optionsStyle: BoxProps = {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingHorizontal: 's24',
+  paddingVertical: 's16',
+};
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+});
