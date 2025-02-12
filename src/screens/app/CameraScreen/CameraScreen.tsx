822abd3988f7b04da9d1ba5a4a62c973fbcb737e
David Rappa <davidrappa@outlook.com>
2025-02-12 12:45:29 -0300

    feat: added camera to CameraScreen

--- src/screens/app/CameraScreen/CameraScreen.tsx
+++ src/screens/app/CameraScreen/CameraScreen.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
 
-import {Box, BoxProps, Icon} from '@components';
-import {useAppSafeArea} from '@hooks';
+import {useIsFocused} from '@react-navigation/native';
+import {Camera, useCameraDevice} from 'react-native-vision-camera';
+
+import {Box, BoxProps, Icon, PermissionManager} from '@components';
+import {useAppSafeArea, useAppState} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 const CAMERA_VIEW = Dimensions.get('screen').width;
@@ -12,36 +15,52 @@ const CONTROL_DIFF = 30;
 export function CameraScreen({navigation}: AppScreenProps<'CameraScreen'>) {
   const {top} = useAppSafeArea();
   const [flashOn, setFlashOne] = useState(false);
+  const device = useCameraDevice('back');
+
+  const isFocused = useIsFocused();
+  const appState = useAppState();
+  const isActive = isFocused && appState === 'active';
 
   function toggleFlash() {
     setFlashOne(prev => !prev);
   }
+
   return (
-    <Box flex={1}>
-      <Box backgroundColor="grayWhite" style={StyleSheet.absoluteFill} />
-
-      <Box flex={1} justifyContent="space-between">
-        <Box {...$controlAreaTop} style={{paddingTop: top}}>
-          <Icon
-            size={20}
-            color="grayWhite"
-            name="arrowLeft"
-            onPress={navigation.goBack}
+    <PermissionManager
+      permissionName="camera"
+      description="Permita o Nubble acessar a camera">
+      <Box flex={1}>
+        {device != null && (
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={isActive}
           />
-          <Icon
-            size={20}
-            color="grayWhite"
-            name={flashOn ? 'flashOn' : 'flashOff'}
-            onPress={toggleFlash}
-          />
-          <Box width={20} />
-        </Box>
+        )}
 
-        <Box {...$controlAreaBottom}>
-          <Icon size={80} name="cameraClick" color="grayWhite" />
+        <Box flex={1} justifyContent="space-between">
+          <Box {...$controlAreaTop} style={{paddingTop: top}}>
+            <Icon
+              size={20}
+              color="grayWhite"
+              name="arrowLeft"
+              onPress={navigation.goBack}
+            />
+            <Icon
+              size={20}
+              color="grayWhite"
+              name={flashOn ? 'flashOn' : 'flashOff'}
+              onPress={toggleFlash}
+            />
+            <Box width={20} />
+          </Box>
+
+          <Box {...$controlAreaBottom}>
+            <Icon size={80} name="cameraClick" color="grayWhite" />
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </PermissionManager>
   );
 }
 
