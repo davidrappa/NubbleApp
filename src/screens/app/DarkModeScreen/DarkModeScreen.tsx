e3a4f64d091ce55a3f74f7eeddd92534497ba42f
David Rappa <davidrappa@outlook.com>
2025-04-03 08:47:01 -0300

    feat: implement radio button selector for dark mode preferences

--- src/screens/app/DarkModeScreen/DarkModeScreen.tsx
+++ src/screens/app/DarkModeScreen/DarkModeScreen.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Screen, Text} from '@components';
+import {RadioButtonSelector, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
+type ThemePreference = 'light' | 'dark' | 'system';
+
+type Option = {
+  label: string;
+  description?: string;
+  themePreference: ThemePreference;
+};
+
+const items: Option[] = [
+  {
+    label: 'Ativado',
+    themePreference: 'dark',
+  },
+  {
+    label: 'Desativado',
+    themePreference: 'light',
+  },
+  {
+    label: 'Padrão do sistema',
+    themePreference: 'system',
+    description:
+      'A aparência será a mesma que você configurou no seu dispositivo',
+  },
+];
+
 export function DarkModeScreen({}: AppScreenProps<'DarkModeScreen'>) {
+  const [selectedItem, setSelectedItem] = useState<Option>();
   return (
     <Screen canGoBack title="Modo escuro">
-      <Text>Dark Mode</Text>
+      <RadioButtonSelector
+        items={items}
+        selectedItem={selectedItem}
+        onSelect={setSelectedItem}
+        labelKey="label"
+        valueKey="themePreference"
+        descriptionKey="description"
+      />
+
+      <Text preset="paragraphLarge" mt="s48">
+        {JSON.stringify(selectedItem, null, 2)}
+      </Text>
     </Screen>
   );
 }
