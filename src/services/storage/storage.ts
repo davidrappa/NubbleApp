073375af70a1438380d7693ea19b192c8f88858e
David Rappa <davidrappa@outlook.com>
2025-04-03 15:33:27 -0300

    feat: update storage initialization to use default MMKVStorage implementation

--- src/services/storage/storage.ts
+++ src/services/storage/storage.ts
@@ -1,11 +1,13 @@
+import {MMKVStorage} from './implementation/mmkvStorage';
+
 export interface Storage {
   getItem: <T = unknown>(key: string) => Promise<T | null>;
   setItem: <T>(key: string, value: T) => Promise<void>;
   removeItem: (key: string) => Promise<void>;
 }
 
-export let storage: Storage;
+export let storage: Storage = MMKVStorage;
 
-export function initializeStorage(storageImpl: Storage) {
-  storage = storageImpl;
+export function initializeStorage(storageImpl?: Storage) {
+  storage = storageImpl || MMKVStorage;
 }
