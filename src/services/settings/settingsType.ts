5756f4f3eda4af7ef2ca61e409335230545723bb
David Rappa <davidrappa@outlook.com>
2025-04-08 08:31:04 -0300

    feat: implement routing logic with useRouter and add onboarding flow support

--- src/services/settings/settingsType.ts
+++ src/services/settings/settingsType.ts
@@ -5,8 +5,10 @@ export type AppColorScheme = 'light' | 'dark';
 export type ThemePreference = AppColorScheme | 'system';
 
 export type SettingsStore = {
+  showOnboarding: boolean;
   appColor: AppColorScheme;
   themePreference: ThemePreference;
   setThemePreference: (themePreference: ThemePreference) => void;
   onSystemChange: (color: ColorSchemeName) => void;
+  finishOnboarding: () => void;
 };
